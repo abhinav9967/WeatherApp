b0e6cc9dc05fd82ea2c284d6d0a6551cc2e7ac08
abhinavkumarrajak <abhinavkumar7362@gmail.com>
2023-09-09 09:51:05 +0530

    files added

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "776786bfec7677e00b62e5f9d053c8c5",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
